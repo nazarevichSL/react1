60d9c9fa0480f6fb66a766ddc29529fdc6f50bdf
nazarevichSL <sportsila83@gmail.com>
2022-01-13 16:57:45 +0200

    delet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {useEffect, useState} from "react";
+import User from "./components/User";
+
 export default function App() {
+
+    let [usersList, setUsersList] = useState([]);
+
+    useEffect (() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {
+                setUsersList(users);
+            })
+    }, []);
     return (
         <div>
-            App
+            {usersList.map(value => <User id={value.id} name={value.name} username={value.username}/>)}
         </div>
     );
 }
---
+++ src/components/User.js
@@ -0,0 +1,12 @@
+
+
+export default function User(props) {
+    let {id, name, username} = props;
+
+
+    return (
+        <div>
+            App
+        </div>
+    );
+}
